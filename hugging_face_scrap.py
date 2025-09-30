df692aabf50869a7406783d438bf69292dd07716
dia2l <dia2lo.ismael@gmail.com>
2025-09-30 22:09:25 +0100

    téléchargement des 3 premiers datasets et du premier modele (Mistral 7B)

--- hugging_face_scrap.py
+++ hugging_face_scrap.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
 import logging
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -8,9 +9,9 @@ logging.info('Début du téléchargement des datasets...')
 logging.info('Loading du dataset : CFDD...')
 claire_french_dialogue = load_dataset("OpenLLM-France/Claire-Dialogue-French-0.1", sample_by="paragraph", streaming=False)
 logging.info('Loading du dataset : Everyday Conversations...')
-everyday_conversation = load_dataset("wraps/everyday-conversations-llama3.1-2k-french", sample_by="paragraph", streaming=False)
+everyday_conversation = load_dataset("wraps/everyday-conversations-llama3.1-2k-french", streaming=False)
 logging.info('Loading du dataset : Comparia Conversations...')
-comparia_conversations = load_dataset("ministere-culture/comparia-conversations")
+comparia_conversations = load_dataset("ministere-culture/comparia-conversations",streaming=False)
 
 logging.info('Ecriture sur disque...')
 logging.info('CFDD...')
@@ -22,8 +23,11 @@ comparia_conversations.save_to_disk("D:/1TRAVAIL/IA/datasets/Comparia")
 
 logging.info('Début du téléchargement des modèles...')
 logging.info('Mistral-7B...')
-model_name = 'mistral-7B'
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model_name = 'mistralai/Mistral-7B-v0.1'
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    dtype=torch.float16,
+    device_map="cuda")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 save_directory = "D:/1TRAVAIL/IA/models/mistral7b"
@@ -31,7 +35,7 @@ save_directory = "D:/1TRAVAIL/IA/models/mistral7b"
 logging.info('Ecriture sur disque...')
 logging.info('Traitement du modèle : Mistral-7B...')
 # Sauvegarde du modèle
-model.save_pretrained(save_directory)
+model.save_pretrained(save_directory,max_shard_size="2GB",safe_serialization=True)
 logging.info('Traitement du tokenizer...')
 # Sauvegarde du tokenizer
 tokenizer.save_pretrained(save_directory)
